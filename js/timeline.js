ec197da5efb6cf7fddec4adcbac8ca1b43250ac7
DavideTonin99 <davide9935@gmail.com>
2017-11-11 20:37:49 +0100

    Fixed timeline

--- js/timeline.js
+++ js/timeline.js
@@ -26,16 +26,16 @@ function draw_timeline() {
     var chart = new google.visualization.Timeline(container);
     var dataTable = new google.visualization.DataTable();
 
-    dataTable.addColumn({ type: 'string', id: 'Referente' });
-    dataTable.addColumn({ type: 'string', id: 'Compito' });
+    dataTable.addColumn({ type: 'string', id: 'Processo' });
+    dataTable.addColumn({ type: 'string', id: 'Referente,Compito' });
     dataTable.addColumn({ type: 'date', id: 'Inizio' });
     dataTable.addColumn({ type: 'date', id: 'Fine' });
 
     timeline = [];
     $(timeline_data).each( function(index) {
         current_process = timeline_data[index];
-        row = [current_process.referente,
-                current_process.processo + ": " +current_process.compito,
+        row = [current_process.processo,
+                current_process.referente + ": "+current_process.compito,
                 new Date(current_process.data_inizio.substring(6,10),current_process.data_inizio.substring(3,5),current_process.data_inizio.substring(0,2)),
                 new Date(current_process.data_fine.substring(6,10),current_process.data_fine.substring(3,5),current_process.data_fine.substring(0,2))];
         timeline.push(row);
